2d0053dc8b9d93955fb4fa2bb349751aa5fc278e
Matt Dickson <matthew.dickson.137@gmail.com>
2019-02-06 01:06:48 -0500

    Correctly renders view

--- tools350/views.py
+++ tools350/views.py
@@ -5,5 +5,5 @@ HTML_ROOT = '/home/mdd36/tools350/static'
 
 
 def root(request):
-    with open(os.path.join(HTML_ROOT, 'index.html'), 'r') as html:
+    with open(os.path.join(HTML_ROOT, 'index', 'index.html'), 'r') as html:
         return HttpResponse(''.join(html.readlines()))
